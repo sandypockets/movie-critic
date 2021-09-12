fc69b5835bd5c83ba84fa121528d6a0ed4c607c9
sandypockets <zac@znacreative.com>
2021-09-11 21:56:53 -0400

    Make lines adjust alongside point data. Add transitions

--- public/javascripts/movie-critic.js
+++ public/javascripts/movie-critic.js
@@ -8,6 +8,26 @@ const scoreScale = d3.scaleLinear()
   .domain([0, 100])
   .range([420, 900])
 
+const imdbLine = d3.line()
+  .x((d, i) => { return scoreScale(d.imdb) })
+  .y((d, i) => { return 40 * i + 20 })
+
+const imdbPath = svg
+  .append("path")
+  .datum(data)
+  .attr("d", imdbLine)
+  .attr("class", "imdb")
+
+const metascoreLine = d3.line()
+  .x((d, i) => { return scoreScale(d.metascore) })
+  .y((d, i) => { return 40 * i + 20 })
+
+const metascorePath = svg
+  .append("path")
+  .datum(data)
+  .attr("d", metascoreLine)
+  .attr("class", "metascore")
+
 const groups = svg
   .selectAll("g.movie")
   .data(data)
@@ -53,25 +73,6 @@ groups
   .attr("r", 8)
   .attr("class", "metascore")
 
-const imdbLine = d3.line()
-  .x((d, i) => { return scoreScale(d.imdb) })
-  .y((d, i) => { return 40 * i + 20 })
-
-const imdbPath = svg
-  .append("path")
-  .datum(data)
-  .attr("d", imdbLine)
-  .attr("class", "imdb")
-
-const metascoreLine = d3.line()
-  .x((d, i) => { return scoreScale(d.metascore) })
-  .y((d, i) => { return 40 * i + 20 })
-
-const metascorePath = svg
-  .append("path")
-  .datum(data)
-  .attr("d", metascoreLine)
-  .attr("class", "metascore")
 
 
 const selectTag = document.querySelector("select")
@@ -89,4 +90,16 @@ selectTag.addEventListener("change", function() {
     .transition()
     .duration(1000)
     .attr("transform", (d, i) => { return `translate(0, ${i * 40})`})
+
+  imdbPath
+    .datum(data, (d, i) => { return d.title })
+    .transition()
+    .duration(1300)
+    .attr("d", imdbLine)
+
+  metascorePath
+    .datum(data, (d, i) => { return d.title })
+    .transition()
+    .duration(1300)
+    .attr("d", metascoreLine)
 })
